c0ba217496e8dab0d220c129f09c86fe8cd0540f
Badyl <pietrasiak.patryk.05@gmail.com>
2019-04-19 22:33:00 +0200

    added migration and updated library.db

---
+++ CrcDotNetCoreHomework/src/CrcDotNetCoreHomeworkWebApi/Migrations/20190419201200_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CrcDotNetCoreHomeworkWebApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Libraries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(nullable: false),
+                    Author = table.Column<string>(nullable: false),
+                    Isbn = table.Column<string>(nullable: false),
+                    Year = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Libraries", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Libraries");
+        }
+    }
+}
