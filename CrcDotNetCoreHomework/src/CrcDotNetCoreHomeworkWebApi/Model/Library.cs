01e0b2129b2ee78b84b5dca86c1df903e4884f39
Badyl <pietrasiak.patryk.05@gmail.com>
2019-04-19 20:19:29 +0200

    added field to library class

--- CrcDotNetCoreHomework/src/CrcDotNetCoreHomeworkWebApi/Model/Library.cs
+++ CrcDotNetCoreHomework/src/CrcDotNetCoreHomeworkWebApi/Model/Library.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,20 @@ namespace CrcDotNetCoreHomeworkWebApi.Model
 {
     public class Library
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        public string Isbn { get; set; }
+
+        [Required]
+        public DateTime Year { get; set; }
     }
 }
