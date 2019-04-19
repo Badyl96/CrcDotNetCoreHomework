166144fd9eaa151d5d670038442331fd3d01d567
Badyl <pietrasiak.patryk.05@gmail.com>
2019-04-19 20:25:24 +0200

    added dbcontext to librarycontext

--- CrcDotNetCoreHomework/src/CrcDotNetCoreHomeworkWebApi/DbContexts/LibraryContext.cs
+++ CrcDotNetCoreHomework/src/CrcDotNetCoreHomeworkWebApi/DbContexts/LibraryContext.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using CrcDotNetCoreHomeworkWebApi.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CrcDotNetCoreHomeworkWebApi.DbContexts
 {
-    public class LibraryContext
+    public class LibraryContext : DbContext
     {
+        public LibraryContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Library> Libraries { get; set; }
     }
 }
